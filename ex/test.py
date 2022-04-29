e53e96dbb7c5babf03f7f16bc8b095700cccef72
Shawn - Alta3 - Ansible <git config --global user.email shawneokeefe@users.noreply.github.com>
2022-04-29 04:32:53 +0000

    v5

---
+++ ex/test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# =========================================
+def TT():
+        input("TEST TEXT")
+def T2(item1, item2):
+        #global X, Y
+        print(f"Original item {item1}")
+        print(f"New item {item2}")
+def TX(item_dest):
+        inv.pop(item_dest)
+# =========================================
+inv =       { 'lint'       : ['item', 'lint', 'Fluff.'],
+              'muffin'     : ['item', 'muffin', 'Yum.']
+            }
+trans =     { 'tin'       : ['item', 'tin', 'Fluff.'],
+              'nut'     : ['item', 'nut', 'hard.']
+            }
+#act_list = ['act1', 'act_call' 'act_name', T2("lint", 'tin')]
+#act_dict = {'act2' : T2(inv[muffin], trans[nut]) 
+act_list = ['act1', 'act_call' 'act_name', T2("lint", "tin"), 'lint', 'tin']
+#     }
+# =========================================
+
+#choice = input("make choice 1")
+#choice2 = input("make choice 2")
+#X = act_list[4]
+#Y = act_list[5]
+X = act_list[3]
+
+X
+
+#act_dict['act2']
+
+#f_list = [
+#            def f1():
+#                print"This is F1"
+#            f1()
+#         ]
+
